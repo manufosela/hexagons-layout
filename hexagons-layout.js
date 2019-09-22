4219d60133b15869e328b23e0b7e7251778233dd
Manuel Juan Fosela Águila <manufosela@gmail.com>
2019-09-22 22:34:14 +0200

    improvment and refactor

--- hexagons-layout.js
+++ hexagons-layout.js
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from 'lit-element';
-import DivHexagon from './div-hexagon';
+import DivHexagon from 'div-hexagon';
 
 /**
  * `hexagons-layout`
@@ -37,6 +37,9 @@ class HexagonsLayout extends LitElement {
         --negroM: #292f33;
         --negroO: #1d1f20;
         --gris: #999;
+
+        /* defined colors */
+        --header-bg: var(--negroM);
         
         box-sizing: border-box;
         font-family: var(--sinSerif);
@@ -57,7 +60,7 @@ class HexagonsLayout extends LitElement {
         margin: .5rem auto;
       }
       header {
-        background: var(--negroM);
+        background: var(--header-bg);
         font-size: calc(1rem + 2vmin);
         font-variant: small-caps;
       }
@@ -67,57 +70,54 @@ class HexagonsLayout extends LitElement {
         letter-spacing: 3px;
         line-height: 1.5;
         text-align: center;
-        box-shadow: inset 0 0 1px 1px var(--negroC);
+        box-shadow: inset 0 0 1px 1px var(--header-bg);
       }
     `;
   }
 
   constructor() {
     super();
-    this.elems = [];
+    this.numElems = 0;
     this.property2 = 2019;
   }
 
-  getHexagon(el) {
-    return html`
-      <article>
-        <figure>
-          <h2>${el.title}</h2>
-          <p>${el.description}</p>
-        </figure>
-        <img src="${el.img}" />
-      </article>`;
-  }
-
   getBloque() {
-    let bloques = Math.ceil(this.elems.length / 7);
+    this.numElems = this.querySelectorAll('div-hexagon').length;
+    let bloques = Math.ceil(this.numElems / 7);
     let bloque = '/* posicionando la colmena */';
     for (let i = 1; i <= bloques; i++) {
       bloque += `
-      article:nth-of-type(${i * 7 - 6}), article:nth-of-type(${i * 7 - 5}), article:nth-of-type(${i * 7 - 4}) {	grid-row: ${i * 2 - 1};}
+      div-hexagon:nth-of-type(${i * 7 - 6}), div-hexagon:nth-of-type(${i * 7 - 5}), div-hexagon:nth-of-type(${i * 7 - 4}) {	grid-row: ${i * 2 - 1}; }
       
-      article:nth-of-type(${i * 7 - 6}) { grid-column: 2 / span 2; }
-      article:nth-of-type(${i * 7 - 5}) { grid-column: 4 / span 2; }
-      article:nth-of-type(${i * 7 - 4}) { grid-column: 6 / span 2; }
+      div-hexagon:nth-of-type(${i * 7 - 6}) { grid-column: 2 / span 2; }
+      div-hexagon:nth-of-type(${i * 7 - 5}) { grid-column: 4 / span 2; }
+      div-hexagon:nth-of-type(${i * 7 - 4}) { grid-column: 6 / span 2; }
       
-      article:nth-of-type(${i * 7 - 3}), article:nth-of-type(${i * 7 - 2}), article:nth-of-type(${i * 7 - 1}), article:nth-of-type(${i * 7}) { grid-row: ${i * 2}; }
+      div-hexagon:nth-of-type(${i * 7 - 3}), div-hexagon:nth-of-type(${i * 7 - 2}), div-hexagon:nth-of-type(${i * 7 - 1}), div-hexagon:nth-of-type(${i * 7}) { grid-row: ${i * 2}; }
 
-      article:nth-of-type(${i * 7 - 3}) { grid-column: 1 / span 2; }
-      article:nth-of-type(${i * 7 - 2}) { grid-column: 3 / span 2; }
-      article:nth-of-type(${i * 7 - 1}) { grid-column: 5 / span 2; }
-      article:nth-of-type(${i * 7}) { grid-column: 7 / span 2; }
+      div-hexagon:nth-of-type(${i * 7 - 3}) { grid-column: 1 / span 2; }
+      div-hexagon:nth-of-type(${i * 7 - 2}) { grid-column: 3 / span 2; }
+      div-hexagon:nth-of-type(${i * 7 - 1}) { grid-column: 5 / span 2; }
+      div-hexagon:nth-of-type(${i * 7}) { grid-column: 7 / span 2; }
       `;
     }
     bloque += `
       /* ajuste vertical */
-      article:nth-of-type(-n+3) {
+      div-hexagon:nth-of-type(-n+3) {
         margin-bottom: calc(var(--height) * -.2);
       }
-      article:nth-of-type(n+8):nth-of-type(-n+${this.elems.length}) {
+      div-hexagon:nth-of-type(n+8):nth-of-type(-n+${this.numElems}) {
         margin-top: calc(var(--height) * -.2);
       }
     `;
     return html`${bloque}`;
+  }
+
+  firstUpdated() {
+    this.querySelectorAll('div-hexagon').forEach((el)=>{
+      this.shadowRoot.querySelector('section').append(el);
+    });
+
   }
 
   render() {
@@ -128,10 +128,7 @@ class HexagonsLayout extends LitElement {
       <header id='follows'>
         <h1>${this.title}</h1>
       </header>
-      <section class='tl'>	
-        ${this.elems.map(element => {
-    return this.getHexagon(element);
-  })}
+      <section class='tl'>       
       </section>
     `;
   }
